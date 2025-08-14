d7a8aec1be80718dcf912f7d472204f853c311f2
TMP1272 <161656592+TMP1272@users.noreply.github.com>
2025-08-15 00:30:57 +0700

    Update Android.mk

--- Native-Build/Src/Jni/SecretsVault/Android.mk
+++ Native-Build/Src/Jni/SecretsVault/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := secretsvault
 LOCAL_SRC_FILES := decrypt.cpp
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../Include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Include
 
 LOCAL_CFLAGS += -Os -ffunction-sections -fdata-sections
 LOCAL_LDFLAGS += -Wl,--gc-sections
