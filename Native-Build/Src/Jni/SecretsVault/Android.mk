62d881da8155c4c99e75ce1110e9a4f6ef6a56e1
TMP1272 <161656592+TMP1272@users.noreply.github.com>
2025-08-15 01:27:34 +0700

    Update Android.mk

--- Native-Build/Src/Jni/SecretsVault/Android.mk
+++ Native-Build/Src/Jni/SecretsVault/Android.mk
@@ -6,7 +6,7 @@ LOCAL_SRC_FILES := decrypt.cpp $(LOCAL_PATH)/../../../../Include/aes.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../Include
 
 LOCAL_STRIP_MODULE := true
-LOCAL_CFLAGS += -Os -flto -ffunction-sections -fdata-sections -fvisibility=hidden
+LOCAL_CFLAGS += -O3 -flto -ffunction-sections -fdata-sections -fvisibility=hidden
 LOCAL_LDFLAGS += -s -flto -Wl,--gc-sections
 LOCAL_LDLIBS += -llog
 
