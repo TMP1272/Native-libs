7091a06aadcaacfde6b5fdb562e7371a79d1d0bb
TMP1272 <161656592+TMP1272@users.noreply.github.com>
2025-08-17 20:38:23 +0700

    Update Android.mk

--- Native-Build/Src/Jni/SecretsVault/FirebaseConfig/Android.mk
+++ Native-Build/Src/Jni/SecretsVault/FirebaseConfig/Android.mk
@@ -9,6 +9,6 @@ LOCAL_SRC_FILES     := decryptfisecoig.cpp ../../../../../Include/aes.c
 LOCAL_C_INCLUDES    := $(LOCAL_PATH)/../../../../../Include
 LOCAL_STRIP_MODULE  := true
 LOCAL_CFLAGS        += -O3 -flto -ffunction-sections -fdata-sections -fvisibility=hidden
-LOCAL_LDFLAGS       += -flto -Wl,--gc-sections
+LOCAL_LDFLAGS       += -s -flto -Wl,--gc-sections
 LOCAL_LDLIBS        += -llog
 include $(BUILD_SHARED_LIBRARY)
