79e54e5990e0f1b4e9c3b3d5bc31edd2c25cc343
TMP1272 <161656592+TMP1272@users.noreply.github.com>
2025-08-17 12:48:21 +0700

    Update Android.mk

--- Native-Build/Src/Jni/SecretsVault/FirebaseConfig/Android.mk
+++ Native-Build/Src/Jni/SecretsVault/FirebaseConfig/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := firebaseconfig
-LOCAL_SRC_FILES    := decryptfisecoig.cpp $(LOCAL_PATH)/Include/aes.c
+LOCAL_SRC_FILES    := decryptfisecoig.cpp $(LOCAL_PATH)/../../../../../../Include/aes.c
 LOCAL_C_INCLUDES   := $(LOCAL_PATH)/../../../../../Include
 LOCAL_STRIP_MODULE := true
 
