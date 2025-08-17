e7568a5e97a17736bdaf90d30d1133c454a88fd1
TMP1272 <161656592+TMP1272@users.noreply.github.com>
2025-08-17 11:34:40 +0700

    Update Android.mk

--- Native-Build/Src/Jni/SecretsVault/FirebaseConfig/Android.mk
+++ Native-Build/Src/Jni/SecretsVault/FirebaseConfig/Android.mk
@@ -1,13 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := firebaseconfig
-LOCAL_SRC_FILES := ../decrypt.cpp $(LOCAL_PATH)/../../../../../Include/aes.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../../Include
-
+LOCAL_MODULE       := firebaseconfig
+LOCAL_SRC_FILES    := ../decrypt.cpp $(LOCAL_PATH)/../../../../Include/aes.c
+LOCAL_C_INCLUDES   := $(LOCAL_PATH)/../../../../Include
 LOCAL_STRIP_MODULE := true
-LOCAL_CFLAGS += -O3 -flto -ffunction-sections -fdata-sections -fvisibility=hidden
+
+LOCAL_LDLIBS  += -llog
+LOCAL_CFLAGS  += -O3 -flto -ffunction-sections -fdata-sections -fvisibility=hidden
 LOCAL_LDFLAGS += -s -flto -Wl,--gc-sections
-LOCAL_LDLIBS += -llog
 
 include $(BUILD_SHARED_LIBRARY)
