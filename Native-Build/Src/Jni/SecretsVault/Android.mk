ea18a02fb61761965870a022bb5db0f678d8ff5a
TMP1272 <161656592+TMP1272@users.noreply.github.com>
2025-08-17 13:06:55 +0700

    Update Android.mk

--- Native-Build/Src/Jni/SecretsVault/Android.mk
+++ Native-Build/Src/Jni/SecretsVault/Android.mk
@@ -1,19 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 
 # ====================
-# SecretsVault
+# secretsvault
 # ====================
-
 include $(CLEAR_VARS)
-LOCAL_MODULE    := secretsvault
-LOCAL_SRC_FILES := decrypt.cpp $(LOCAL_PATH)/../../../../Include/aes.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../Include
-
-LOCAL_STRIP_MODULE := true
-LOCAL_CFLAGS += -O3 -flto -ffunction-sections -fdata-sections -fvisibility=hidden
-LOCAL_LDFLAGS += -s -flto -Wl,--gc-sections
-LOCAL_LDLIBS += -llog
-
+LOCAL_MODULE        := secretsvault
+LOCAL_SRC_FILES     := decrypt.cpp ../../../../Include/aes.c
+LOCAL_C_INCLUDES    := $(LOCAL_PATH)/../../../../Include
+LOCAL_STRIP_MODULE  := true
+LOCAL_CFLAGS        += -O3 -flto -ffunction-sections -fdata-sections -fvisibility=hidden
+LOCAL_LDFLAGS       += -s -flto -Wl,--gc-sections
+LOCAL_LDLIBS        += -llog
 include $(BUILD_SHARED_LIBRARY)
 
+# ====================
+# Include firebaseconfig module
+# ====================
 include $(LOCAL_PATH)/FirebaseConfig/Android.mk
